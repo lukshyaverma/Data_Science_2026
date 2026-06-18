658b1494729e341d610fae6f7bf3639d12980c36
lukshyaverma <67636863+lukshyaverma@users.noreply.github.com>
2026-06-18 18:40:08 +0530

    Create 01_Data_Load.sql

---
+++ 01_Data_Load.sql
@@ -0,0 +1,40 @@
+create database if not exists fraud_project;
+use fraud_project;
+
+drop table main_data;
+create table main_data(
+	txn_key int primary key,
+	trans_date_trans_time varchar(20),
+	cc_num bigint,
+	merchant varchar(50),
+	category varchar(20),
+	amount double,
+	first_name varchar(20),
+	last_name varchar(20),
+	gender varchar(1),
+	street varchar(50),
+	city varchar(50),
+	state varchar(20),
+	zip bigint,
+	lat decimal,
+	lon decimal,
+	city_pop bigint,
+	job varchar(100),
+	dob varchar(20),
+	trans_num varchar(50),
+	unix_time bigint,
+	merch_lat decimal,
+	merch_long decimal,
+	fraud_ind int
+);
+
+DESCRIBE main_data;
+
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/kaggle_fraud.csv'
+INTO TABLE main_data
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+select * from main_data limit 5;
